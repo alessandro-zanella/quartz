877940a52736c1badbc1f862067abbbcbb26d83e
Alessandro Zanella <zan15592@gmail.com>
2025-01-23 04:01:47 +0200

    first adaptation of quartz for my purposes

---
+++ content/index.md
@@ -0,0 +1,12 @@
+---
+title: Home
+---
+
+
+
+# Alessandro Zanella's Portfolio Site
+
+## [[Psalms 1]]
+
+This is a blank Quartz installation.
+See the [documentation](https://quartz.jzhao.xyz) for how to get started.
--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,6 +1,5 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
-import { version } from "../../package.json"
 import { i18n } from "../i18n"
 
 interface Options {
@@ -10,20 +9,41 @@ interface Options {
 export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
-    const links = opts?.links ?? []
+
     return (
-      <footer class={`${displayClass ?? ""}`}>
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
+      <footer class={`${displayClass ?? ""} footer`}>
+        <div class="footer-column">
+          <p>RAINY DAWN ENTERTAINMENT SRL</p>
+          <p>rainydawnentertainment@gmail.com</p>
+        </div>
+        <div class="footer-column">
+          <ul class="social-links">
             <li>
-              <a href={link}>{text}</a>
+              <a href="https://www.facebook.com/yourprofile" target="_blank" rel="noopener noreferrer">
+                <img src="/static/socials/facebook.png" alt="Facebook" width="24" height="24" />
+              </a>
             </li>
-          ))}
-        </ul>
+            <li>
+              <a href="https://www.instagram.com/yourprofile" target="_blank" rel="noopener noreferrer">
+                <img src="/static/socials/facebook.png" alt="Instagram" width="24" height="24" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/yourprofile" target="_blank" rel="noopener noreferrer">
+                <img src="/static/socials/facebook.png" alt="YouTube" width="24" height="24" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.twitter.com/yourprofile" target="_blank" rel="noopener noreferrer">
+                <img src="/static/socials/facebook.png" alt="Twitter" width="24" height="24" />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="footer-column">
+          <p>Let's Stick Together</p>
+        </div>
+        <p>© {year}</p>
       </footer>
     )
   }
